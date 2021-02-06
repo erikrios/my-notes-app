b8276000bd44dddb1630529d6c8b3ddfdbc8793c
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-06 23:14:30 +0700

    Integrated the recycler view

--- app/src/main/java/com/erikriosetiawan/mynotesapp/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/mynotesapp/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.erikriosetiawan.mynotesapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.erikriosetiawan.mynotesapp.adapter.NoteAdapter
+import com.erikriosetiawan.mynotesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: NoteAdapter
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = "Notes"
+
+        binding.rvNotes.layoutManager = LinearLayoutManager(this)
+        binding.rvNotes.setHasFixedSize(true)
+        adapter = NoteAdapter(this)
+        binding.rvNotes.adapter = adapter
     }
 }
