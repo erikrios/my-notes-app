58295dbde68eed4437fcc06ae81fd29cb551bb7b
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-06 22:37:24 +0700

    Refactor the code

--- app/src/main/java/com/erikriosetiawan/mynotesapp/entity/Note.kt
+++ app/src/main/java/com/erikriosetiawan/mynotesapp/entity/Note.kt
@@ -6,7 +6,7 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class Note(
     var id: Int = 0,
-    var title: String,
+    var title: String? = null,
     var description: String? = null,
     var date: String? = null
 ) : Parcelable
