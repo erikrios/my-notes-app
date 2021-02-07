d567d1a7e2d60bf598d97cc2cd20074c5a224641
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-07 19:56:39 +0700

    Implements the content provider

--- app/src/main/java/com/erikriosetiawan/mynotesapp/provider/NoteProvider.kt
+++ app/src/main/java/com/erikriosetiawan/mynotesapp/provider/NoteProvider.kt
@@ -2,41 +2,84 @@ package com.erikriosetiawan.mynotesapp.provider
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.Context
+import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
+import com.erikriosetiawan.mynotesapp.NoteHelper
+import com.erikriosetiawan.mynotesapp.db.DatabaseContract.AUTHORITY
+import com.erikriosetiawan.mynotesapp.db.DatabaseContract.NoteColumns.Companion.CONTENT_URI
+import com.erikriosetiawan.mynotesapp.db.DatabaseContract.NoteColumns.Companion.TABLE_NAME
 
 class NoteProvider : ContentProvider() {
 
-    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
-        TODO("Implement this to handle requests to delete one or more rows")
-    }
+    companion object {
+        private const val NOTE = 1
+        private const val NOTE_ID = 2
+        private val sUriMatcher = UriMatcher(UriMatcher.NO_MATCH)
+        private lateinit var noteHelper: NoteHelper
 
-    override fun getType(uri: Uri): String? {
-        TODO(
-            "Implement this to handle requests for the MIME type of the data" +
-                    "at the given URI"
-        )
-    }
+        init {
+            // content://com.erikriosetiawan.mynotesapp/note
+            sUriMatcher.addURI(AUTHORITY, TABLE_NAME, NOTE)
 
-    override fun insert(uri: Uri, values: ContentValues?): Uri? {
-        TODO("Implement this to handle requests to insert a new row.")
+            // content://com.erikriosetiawan.mynotesapp/note/id
+            sUriMatcher.addURI(AUTHORITY, "$TABLE_NAME/#", NOTE_ID)
+        }
     }
 
     override fun onCreate(): Boolean {
-        TODO("Implement this to initialize your content provider on startup.")
+        noteHelper = NoteHelper.getInstance(context as Context)
+        noteHelper.open()
+        return true
     }
 
     override fun query(
         uri: Uri, projection: Array<String>?, selection: String?,
         selectionArgs: Array<String>?, sortOrder: String?
     ): Cursor? {
-        TODO("Implement this to handle query requests from clients.")
+        return when (sUriMatcher.match(uri)) {
+            NOTE -> noteHelper.queryAll()
+            NOTE_ID -> noteHelper.queryById(uri.lastPathSegment.toString())
+            else -> null
+        }
+    }
+
+    override fun getType(uri: Uri): String? {
+        return null
+    }
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        val added: Long = when (NOTE) {
+            sUriMatcher.match(uri) -> noteHelper.insert(values)
+            else -> 0
+        }
+
+        context?.contentResolver?.notifyChange(CONTENT_URI, null)
+        return Uri.parse("$CONTENT_URI/$added")
     }
 
     override fun update(
         uri: Uri, values: ContentValues?, selection: String?,
         selectionArgs: Array<String>?
     ): Int {
-        TODO("Implement this to handle requests to update one or more rows.")
+        val updated: Int = when (NOTE_ID) {
+            sUriMatcher.match(uri) -> noteHelper.update(uri.lastPathSegment.toString(), values)
+            else -> 0
+        }
+
+        context?.contentResolver?.notifyChange(CONTENT_URI, null)
+        return updated
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
+        val deleted: Int = when (NOTE_ID) {
+            sUriMatcher.match(uri) -> noteHelper.deleteById(uri.lastPathSegment.toString())
+            else -> 0
+        }
+
+        context?.contentResolver?.notifyChange(CONTENT_URI, null)
+
+        return deleted
     }
 }
