ccfc10c01f5eed0cab798a78c7eebcfb39e38b19
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-07 19:35:29 +0700

    Add content uri

--- app/src/main/java/com/erikriosetiawan/mynotesapp/db/DatabaseContract.kt
+++ app/src/main/java/com/erikriosetiawan/mynotesapp/db/DatabaseContract.kt
@@ -1,16 +1,26 @@
 package com.erikriosetiawan.mynotesapp.db
 
+import android.net.Uri
 import android.provider.BaseColumns
 
-internal class DatabaseContract {
+object DatabaseContract {
 
-    internal class NoteColumns : BaseColumns {
+    const val AUTHORITY = "com.erikriosetiawan.mynotesapp"
+    const val SCHEME = "content"
+
+    class NoteColumns : BaseColumns {
         companion object {
             const val TABLE_NAME = "note"
             const val _ID = "_id"
             const val TITLE = "title"
             const val DESCRIPTION = "description"
             const val DATE = "date"
+
+            // To create URI content://com.erikriosetiawan.mynotesapp/note
+            val CONTENT_URI: Uri = Uri.Builder().scheme(SCHEME)
+                .authority(AUTHORITY)
+                .appendPath(TABLE_NAME)
+                .build()
         }
     }
 }
